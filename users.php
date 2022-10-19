a50333a29b975cf6ec478f0eff3b18efa6570a79
x10102 <x10102.ot@gmail.com>
2022-10-19 20:04:54 +0200

    Add new file nebo neco

---
+++ users.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="cs">
+<head>
+    <meta charset="utf-8">
+    <title>uwu</title>
+    <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="styles/navbar.css">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet">
+    <style>
+        :root {
+            --text-color: white;
+        }
+    </style>
+</head>
+<body>
+
+<h1 id="title">Správa Uživatelů</h1>
+<table>
+    <tr>
+        <th>_</th>
+        <th>Jméno</th>
+        <th>Oprávnění</th>
+    </tr>
+    <tr>
+        <td><input type="checkbox" name="chb1"></td>
+    </tr>
+
+</table>
+
+</body>
+</html>
