d7550abb7f4faafcecac413467175288fd77ca4a
x10102 <x10102.ot@gmail.com>
2022-11-02 17:16:55 +0100

    No

--- navbar.js
+++ navbar.js
@@ -1,10 +1,10 @@
 var path = window.location.pathname;
 var current = path.split("/").pop();
 
-var pages = {'index.html': 'Hlavní Strana',
-            'contact.html': 'Kontakty',
+var pages = {'index.php': 'Hlavní Strana',
+            'contact.php': 'Kontakty',
             'login.php': 'Přihlášení',
-            'personal.html': 'Profil Majitele'
+            'personal.php': 'Profil Majitele'
             };
 
 navbar = document.getElementById("navbar");
